1710889cb6e9504e3eea70d89f65d1b334739c6f
hoangngm <hoangngm208@gmail.com>
2026-07-24 11:43:12 +0700

    feat: init migration and seeding for the department

---
+++ app/Models/Department.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Attributes\Fillable;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+#[Fillable(['name', 'description'])]
+class Department extends Model
+{
+    /** @use HasFactory<\Database\Factories\DepartmentFactory> */
+    use HasFactory;
+    use SoftDeletes;
+}
---
+++ database/seeders/DepartmentSeeder.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Department;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class DepartmentSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        // fresh dataa
+        Department::truncate();
+        // create 10 departments
+        Department::factory(5)->create();
+    }
+}
